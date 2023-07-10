19eba9c5a8154aa0972643a5e2f18a9c19ba0d0b
levins55 <levinsbscit2019@gmail.com>
2023-07-10 11:15:58 +0530

    created the  port 8000

---
+++ Unit1/Coding Activity 1/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.get('/',(req,res) => {
+    res.send('hii i am LEVIN S ')
+})
+
+app.get('/data', (req, res) => {
+  res.send('This my frist set up the express program using git');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
